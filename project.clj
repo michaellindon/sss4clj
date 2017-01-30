b63513ae8029b58d24fe13c3025dcde5b900c6af
Michael Lindon <michael.s.lindon@gmail.com>
2017-01-30 18:56:47 -0500

    exclusions in project.clj

--- project.clj
+++ project.clj
@@ -4,9 +4,9 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [incanter "1.9.1"]
-                 [net.mikera/core.matrix "0.57.0"]
-                 [net.mikera/vectorz-clj "0.45.0"]
-                 [org.clojure/test.check "0.9.0"]
-		 [org.clojure/math.combinatorics "0.1.3"]
-                 [org.clojure/data.priority-map "0.0.7"]])
+                 [incanter "1.9.1" :exclusions [org.clojure/clojure]]
+                 [net.mikera/core.matrix "0.57.0" :exclusions [org.clojure/clojure]]
+                 [net.mikera/vectorz-clj "0.45.0" :exclusions [org.clojure/clojure]]
+                 [org.clojure/test.check "0.9.0" :exclusions [org.clojure/clojure]]
+		 [org.clojure/math.combinatorics "0.1.3" :exclusions [org.clojure/clojure]]
+                 [org.clojure/data.priority-map "0.0.7" :exclusions [org.clojure/clojure]]])
